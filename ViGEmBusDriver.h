7a4347242365938ff06d1b2f93090dc37d2a4e86
Benjamin Höglinger <nefarius@dhmx.at>
2017-08-28 14:54:25 +0200

    Added comments

--- ViGEmBusDriver.h
+++ ViGEmBusDriver.h
@@ -25,7 +25,7 @@ SOFTWARE.
 
 // {A77BC4D5-6AF7-4E69-8DC4-6B88A6028CE6}
 // ReSharper disable once CppMissingIncludeGuard
-DEFINE_GUID(GUID_VIGEM_INTERFACE_STANDARD,
+DEFINE_GUID(GUID_VIGEM_INTERFACE_PDO,
     0xA77BC4D5, 0x6AF7, 0x4E69, 0x8D, 0xC4, 0x6B, 0x88, 0xA6, 0x02, 0x8C, 0xE6);
 
 // {A8BA2D1F-894F-464A-B0CE-7A0C8FD65DF1}
@@ -37,6 +37,9 @@ DEFINE_GUID(GUID_DEVCLASS_VIGEM_RAWPDO,
 DECLARE_GLOBAL_CONST_UNICODE_STRING(VigemNtDeviceName, L"\\Device\\ViGEmBus");
 DECLARE_GLOBAL_CONST_UNICODE_STRING(VigemDosDeviceName, L"\\DosDevices\\ViGEmBus");
 
+//
+// Describes the current stage a PDO completed
+// 
 typedef enum _VIGEM_PDO_STAGE
 {
     ViGEmPdoCreate,
@@ -45,19 +48,14 @@ typedef enum _VIGEM_PDO_STAGE
 
 } VIGEM_PDO_STAGE, *PVIGEM_PDO_STAGE;
 
+//
+// PDO stage result callback definition
+// 
 typedef
 VOID
 (*PVIGEM_BUS_PDO_STAGE_RESULT)(
     _In_ PINTERFACE InterfaceHeader,
     _In_ VIGEM_PDO_STAGE Stage,
-    _In_ ULONG Serial,
-    _In_ NTSTATUS Status
-    );
-
-typedef
-VOID
-(*PVIGEM_BUS_PDO_INTERNAL_IO_CONTROL_RESULT)(
-    _In_ PINTERFACE InterfaceHeader,
     _In_ ULONG Serial,
     _In_ NTSTATUS Status
     );
@@ -68,6 +66,9 @@ typedef struct _VIGEM_BUS_INTERFACE {
     // 
     INTERFACE InterfaceHeader;
 
+    //
+    // PDO stage result callback
+    // 
     PVIGEM_BUS_PDO_STAGE_RESULT BusPdoStageResult;
 
 } VIGEM_BUS_INTERFACE, *PVIGEM_BUS_INTERFACE;
