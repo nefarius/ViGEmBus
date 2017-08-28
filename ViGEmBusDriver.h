34a15aac15161b25cbf725987229cf823220a33d
Benjamin Höglinger <nefarius@dhmx.at>
2017-08-28 13:49:15 +0200

    Implemented BUS_PDO_REPORT_STAGE_RESULT

--- ViGEmBusDriver.h
+++ ViGEmBusDriver.h
@@ -28,14 +28,35 @@ SOFTWARE.
 DEFINE_GUID(GUID_VIGEM_INTERFACE_STANDARD,
     0xA77BC4D5, 0x6AF7, 0x4E69, 0x8D, 0xC4, 0x6B, 0x88, 0xA6, 0x02, 0x8C, 0xE6);
 
+// {A8BA2D1F-894F-464A-B0CE-7A0C8FD65DF1}
+DEFINE_GUID(GUID_DEVCLASS_VIGEM_RAWPDO,
+    0xA8BA2D1F, 0x894F, 0x464A, 0xB0, 0xCE, 0x7A, 0x0C, 0x8F, 0xD6, 0x5D, 0xF1);
+
 #pragma once
 
 DECLARE_GLOBAL_CONST_UNICODE_STRING(VigemNtDeviceName, L"\\Device\\ViGEmBus");
 DECLARE_GLOBAL_CONST_UNICODE_STRING(VigemDosDeviceName, L"\\DosDevices\\ViGEmBus");
 
+typedef enum _VIGEM_PDO_STAGE
+{
+    ViGEmPdoCreate,
+    ViGEmPdoPrepareHardware,
+    ViGEmPdoUsbGetStatusFromDevice
+
+} VIGEM_PDO_STAGE, *PVIGEM_PDO_STAGE;
+
 typedef
-NTSTATUS
-(*PVIGEM_BUS_CREATE_PDO_RESULT)(
+VOID
+(*PVIGEM_BUS_PDO_STAGE_RESULT)(
+    _In_ PINTERFACE InterfaceHeader,
+    _In_ VIGEM_PDO_STAGE Stage,
+    _In_ ULONG Serial,
+    _In_ NTSTATUS Status
+    );
+
+typedef
+VOID
+(*PVIGEM_BUS_PDO_INTERNAL_IO_CONTROL_RESULT)(
     _In_ PINTERFACE InterfaceHeader,
     _In_ ULONG Serial,
     _In_ NTSTATUS Status
@@ -45,12 +66,21 @@ typedef struct _VIGEM_BUS_INTERFACE {
     // 
     // Standard interface header, must be present
     // 
-    INTERFACE                       InterfaceHeader;
+    INTERFACE InterfaceHeader;
 
-    PVIGEM_BUS_CREATE_PDO_RESULT    BusCreatePdoResult;
+    PVIGEM_BUS_PDO_STAGE_RESULT BusPdoStageResult;
 
 } VIGEM_BUS_INTERFACE, *PVIGEM_BUS_INTERFACE;
 
 #define VIGEM_BUS_INTERFACE_VERSION      1
 
-#include "ViGEmBusShared.h"
+VOID FORCEINLINE BUS_PDO_REPORT_STAGE_RESULT(
+    VIGEM_BUS_INTERFACE Interface, 
+    VIGEM_PDO_STAGE Stage,
+    ULONG Serial,
+    NTSTATUS Status
+)
+{
+    (*Interface.BusPdoStageResult)(&Interface.InterfaceHeader, Stage, Serial, Status);
+}
+
