cb3d90f2fc5779e1be9c63a50f1f2591ec48601b
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2018-09-29 22:48:18 +0200

    Implemented version stamping in build script

--- build/Build.cs
+++ build/Build.cs
@@ -1,46 +1,49 @@
 using System;
-using System.Linq;
+using System.IO;
+using JsonConfig;
 using Nuke.Common;
+using Nuke.Common.BuildServers;
 using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Tools.MSBuild;
-using static Nuke.Common.EnvironmentInfo;
+using Vestris.ResourceLib;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 
-class Build : NukeBuild
+internal class Build : NukeBuild
 {
-    public static int Main () => Execute<Build>(x => x.Compile);
+    [GitRepository] private readonly GitRepository GitRepository;
+    [GitVersion] private readonly GitVersion GitVersion;
 
-    [Solution] readonly Solution Solution;
-    [GitRepository] readonly GitRepository GitRepository;
+    [Solution("ViGEmClient.sln")] private readonly Solution Solution;
 
-    AbsolutePath SourceDirectory => RootDirectory / "src";
-    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    private AbsolutePath SourceDirectory => RootDirectory / "src";
+    private AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
-    Target Clean => _ => _
+    private Target Clean => _ => _
         .Executes(() =>
         {
             DeleteDirectories(GlobDirectories(SourceDirectory, "**/bin", "**/obj"));
             EnsureCleanDirectory(ArtifactsDirectory);
         });
 
-    Target Restore => _ => _
+    private Target Restore => _ => _
         .DependsOn(Clean)
         .Executes(() =>
         {
             MSBuild(s => s
-                .SetTargetPath(SolutionFile)
+                .SetTargetPath(Solution)
                 .SetTargets("Restore"));
         });
 
-    Target Compile => _ => _
+    private Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
             MSBuild(s => s
-                .SetTargetPath(SolutionFile)
+                .SetTargetPath(Solution)
                 .SetTargets("Rebuild")
                 .SetConfiguration(Configuration)
                 .SetMaxCpuCount(Environment.ProcessorCount)
@@ -48,12 +51,26 @@ class Build : NukeBuild
                 .SetTargetPlatform(MSBuildTargetPlatform.x64));
 
             MSBuild(s => s
-                .SetTargetPath(SolutionFile)
+                .SetTargetPath(Solution)
                 .SetTargets("Rebuild")
                 .SetConfiguration(Configuration)
                 .SetMaxCpuCount(Environment.ProcessorCount)
                 .SetNodeReuse(IsLocalBuild)
                 .SetTargetPlatform(MSBuildTargetPlatform.x86));
+
+            if (Configuration.Equals("release_dll", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var version =
+                    new Version(IsLocalBuild ? GitVersion.GetNormalizedFileVersion() : AppVeyor.Instance.BuildVersion);
+
+                StampVersion(
+                    Path.Combine(RootDirectory, @"bin\release\x64\ViGEmClient.dll"),
+                    version);
+
+                StampVersion(
+                    Path.Combine(RootDirectory, @"bin\release\x86\ViGEmClient.dll"),
+                    version);
+            }
         });
 
     private Target Pack => _ => _
@@ -61,10 +78,49 @@ class Build : NukeBuild
         .Executes(() =>
         {
             MSBuild(s => s
-                .SetTargetPath(SolutionFile)
+                .SetTargetPath(Solution)
                 .SetTargets("Restore", "Pack")
                 .SetPackageOutputPath(ArtifactsDirectory)
                 .SetConfiguration(Configuration)
                 .EnableIncludeSymbols());
         });
+
+    public static int Main()
+    {
+        return Execute<Build>(x => x.Compile);
+    }
+
+    private static void StampVersion(string path, Version version)
+    {
+        var versionResource = new VersionResource
+        {
+            FileVersion = version.ToString(),
+            ProductVersion = version.ToString()
+        };
+
+        var stringFileInfo = new StringFileInfo();
+        versionResource[stringFileInfo.Key] = stringFileInfo;
+        var stringFileInfoStrings = new StringTable
+        {
+            LanguageID = 1033,
+            CodePage = 1200
+        };
+        stringFileInfo.Strings.Add(stringFileInfoStrings.Key, stringFileInfoStrings);
+        stringFileInfoStrings["CompanyName"] = Config.Global.Version.CompanyName;
+        stringFileInfoStrings["FileDescription"] = Config.Global.Version.FileDescription;
+        stringFileInfoStrings["FileVersion"] = version.ToString();
+        stringFileInfoStrings["InternalName"] = Config.Global.Version.InternalName;
+        stringFileInfoStrings["LegalCopyright"] = Config.Global.Version.LegalCopyright;
+        stringFileInfoStrings["OriginalFilename"] = Config.Global.Version.OriginalFilename;
+        stringFileInfoStrings["ProductName"] = Config.Global.Version.ProductName;
+        stringFileInfoStrings["ProductVersion"] = version.ToString();
+
+        var varFileInfo = new VarFileInfo();
+        versionResource[varFileInfo.Key] = varFileInfo;
+        var varFileInfoTranslation = new VarTable("Translation");
+        varFileInfo.Vars.Add(varFileInfoTranslation.Key, varFileInfoTranslation);
+        varFileInfoTranslation[ResourceUtil.USENGLISHLANGID] = 1300;
+
+        versionResource.SaveTo(path);
+    }
 }
