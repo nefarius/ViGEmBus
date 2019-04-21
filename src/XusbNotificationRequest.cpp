9071d6c3afd0533a4c9793e8b498dacf85545576
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-04-21 16:57:05 +0200

    Created class XusbNotificationRequest

---
+++ src/XusbNotificationRequest.cpp
@@ -0,0 +1,28 @@
+﻿#include "XusbNotificationRequest.h"
+
+
+XusbNotificationRequest::XusbNotificationRequest(
+    HANDLE bus,
+    ULONG serial,
+    HANDLE wait
+) : parent_bus(bus),
+    payload(), transferred(0),
+    overlapped()
+{
+    overlapped.hEvent = wait;
+    XUSB_REQUEST_NOTIFICATION_INIT(&payload, serial);
+}
+
+void XusbNotificationRequest::requestAsync()
+{
+    DeviceIoControl(
+        parent_bus,
+        IOCTL_XUSB_REQUEST_NOTIFICATION,
+        &payload,
+        payload.Size,
+        &payload,
+        payload.Size,
+        &transferred,
+        &overlapped
+    );
+}
---
+++ src/XusbNotificationRequest.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <Windows.h>
+#include "ViGEm/km/BusShared.h"
+
+class XusbNotificationRequest
+{
+private:
+    HANDLE parent_bus;
+    XUSB_REQUEST_NOTIFICATION payload;
+    DWORD transferred;
+    OVERLAPPED overlapped;
+
+public:
+    XusbNotificationRequest(HANDLE bus, ULONG serial, HANDLE wait);
+
+    void requestAsync();
+};
