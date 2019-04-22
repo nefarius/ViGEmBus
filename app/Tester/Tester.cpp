ca441cf623cc6d9a3137ce7727878ec1b1dd4203
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-04-22 14:11:06 +0200

    Fixed broken tester code

--- app/Tester/Tester.cpp
+++ app/Tester/Tester.cpp
@@ -7,8 +7,6 @@
 
 #include <mutex>
 #include <iostream>
-#include "../../../vcpkg/installed/x86-windows-static/include/boost/thread/thread_only.hpp"
-#include "../../../vcpkg/installed/x86-windows-static/include/boost/thread/once.hpp"
 
 static std::mutex m;
 
@@ -53,21 +51,10 @@ int main()
     {
         ret = vigem_target_x360_update(client, x360, report);
         report.bLeftTrigger++;
-        boost::detail::win32::sleep(10);
+        Sleep(10);
     }
 
     vigem_target_remove(client, x360);
     vigem_target_free(x360);
     vigem_free(client);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
