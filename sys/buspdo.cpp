5355deb20cb7728b538174a7dfaeca11f2f977cb
Jason Hart <megadrago88@gmail.com>
2020-05-11 19:24:38 -0400

    Update and/or add license as needed

--- sys/buspdo.cpp
+++ sys/buspdo.cpp
@@ -3,7 +3,7 @@
 *
 * MIT License
 *
-* Copyright (c) 2016-2019 Nefarius Software Solutions e.U. and Contributors
+* Copyright (c) 2016-2020 Nefarius Software Solutions e.U. and Contributors
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
